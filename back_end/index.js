49cdbc5fe6c64f829eaba4a1a2d4314e5101f5af
Poliana Elizabeth <polianaelizabeth@gmail.com>
2025-04-12 14:51:42 -0300

    limpeza e organizacao de codigo

--- back_end/index.js
+++ back_end/index.js
@@ -1,5 +1,4 @@
 const express = require ('express')
-const { stat } = require('fs')
 const app = express()
 const path = require('path')
 
@@ -7,7 +6,7 @@ const path = require('path')
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '/front_end')))
 app.get("/api/tarefas",(req,res)=>{
-    req.json([])
+    res.json([])
 })
 
 app.listen(8000,()=>{
