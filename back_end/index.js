f8376c9dd2e16b8c9cdf7221c2201ff3b39b12b7
Poliana Elizabeth <polianaelizabeth@gmail.com>
2025-04-12 14:34:11 -0300

    organzacao de pastas

---
+++ back_end/index.js
@@ -0,0 +1,16 @@
+const express = require ('express')
+const { stat } = require('fs')
+const app = express()
+const path = require('path')
+
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/front_end')))
+app.get("/api/tarefas",(req,res)=>{
+    req.json([])
+})
+
+app.listen(8000,()=>{
+    console.log("api já fiz?rodando no servidor local")
+
+})
